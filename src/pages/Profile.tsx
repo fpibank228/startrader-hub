430a700737383657d76400c88304602d17919065
hostnes <lapanik2708@gmail.com>
2025-04-23 22:06:52 +0300

    dist

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -371,7 +371,14 @@ const Profile = () => {
                                         <div className="flex flex-col">
                                             <h1 className="text-white/70 mb-1 font-bold text-xl">Баланс</h1>
                                             <div className="flex items-center justify-between">
-                                                <h3 className="text-2xl font-bold text-white">{userInfo.user_details.balance} TON</h3>
+                                                <h3 className="flex gap-1 text-2xl font-bold text-white">{userInfo.user_details.balance}
+                                                    <svg width="32" height="32" viewBox="0 0 14 15" fill="#546CF3"
+                                                         xmlns="http://www.w3.org/2000/svg">
+                                                        <path fillRule="evenodd" clipRule="evenodd"
+                                                              d="M6.63869 12.1902L3.50621 14.1092C3.18049 14.3087 2.75468 14.2064 2.55515 13.8807C2.45769 13.7216 2.42864 13.5299 2.47457 13.3491L2.95948 11.4405C3.13452 10.7515 3.60599 10.1756 4.24682 9.86791L7.6642 8.22716C7.82352 8.15067 7.89067 7.95951 7.81418 7.80019C7.75223 7.67116 7.61214 7.59896 7.47111 7.62338L3.66713 8.28194C2.89387 8.41581 2.1009 8.20228 1.49941 7.69823L0.297703 6.69116C0.00493565 6.44581 -0.0335059 6.00958 0.211842 5.71682C0.33117 5.57442 0.502766 5.48602 0.687982 5.47153L4.35956 5.18419C4.61895 5.16389 4.845 4.99974 4.94458 4.75937L6.36101 1.3402C6.5072 0.987302 6.91179 0.819734 7.26469 0.965925C7.43413 1.03612 7.56876 1.17075 7.63896 1.3402L9.05539 4.75937C9.15496 4.99974 9.38101 5.16389 9.6404 5.18419L13.3322 5.47311C13.713 5.50291 13.9975 5.83578 13.9677 6.2166C13.9534 6.39979 13.8667 6.56975 13.7269 6.68896L10.9114 9.08928C10.7131 9.25826 10.6267 9.52425 10.6876 9.77748L11.5532 13.3733C11.6426 13.7447 11.414 14.1182 11.0427 14.2076C10.8642 14.2506 10.676 14.2208 10.5195 14.1249L7.36128 12.1902C7.13956 12.0544 6.8604 12.0544 6.63869 12.1902Z"
+                                                              fill="ddd"></path>
+                                                    </svg>
+                                                </h3>
                                                 <Button
                                                     onClick={handleTopUpClick}
                                                     variant="secondary"
@@ -392,7 +399,7 @@ const Profile = () => {
                                             <div
                                                 className="bg-white/5 rounded-lg p-4 text-center flex items-center justify-center">
                                                 <div>
-                                                    <p className="text-4xl font-bold">50%</p>
+                                                <p className="text-4xl font-bold">50%</p>
                                                     <p className="text-base text-white/70">твой процент</p>
                                                 </div>
                                             </div>
